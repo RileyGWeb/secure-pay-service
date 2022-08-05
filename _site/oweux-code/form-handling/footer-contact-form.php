40e2217ec3243c9fa07b611f874cb63c88abf1fc
Coaster132 <99291390+RileyGWeb@users.noreply.github.com>
2022-08-05 10:03:17 -0700

    I don't know

--- _site/oweux-code/form-handling/footer-contact-form.php
+++ _site/oweux-code/form-handling/footer-contact-form.php
@@ -31,6 +31,31 @@ if (isset($_POST) && !empty($_POST)) {
 
     $form_json_log_array[$form_submission_unique] = $_POST;
 
+    $recipient_array = array(
+
+
+        "contact@speedy-sites.com" => "Riley Goodman",
+        "fernando@oweux.com" => "Fernando Venturello",
+        //"info@securepayservice.com" => "info@securepayservice.com",
+
+    );
+
+
+    $text_content = 'Please use an HTML capable client to read this email';
+
+    $html_content = array();
+
+    foreach ($_POST as $_POST_label => $_POST_value) {
+
+        $html_content[] = "<p><strong>" . str_replace(array('-', '_'), ' ', $_POST_label) . "</strong>: {$_POST_value}</p>";
+
+    }
+
+    $html_content = print_r(implode('', $html_content), true);
+
+    require('../sendgrid/code/send-script.php');
+
+
 } else {
 
     $form_json_log_array[$form_submission_unique] = array(
@@ -45,5 +70,6 @@ if (isset($_POST) && !empty($_POST)) {
 //echo "<pre>" . print_r($form_json_log_array, true) . "</pre>";
 
 file_put_contents($form_json_log_file, json_encode($form_json_log_array));
+
 
 require("../form-output/{$form_name}-thank-you-block.php");
